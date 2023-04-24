a0d9f814cc1cf36070ef1aaed95032290e3d8bbd
mboot-github <130295084+mboot-github@users.noreply.github.com>
2023-04-24 15:36:38 +0200

    cleanup testdata

---
+++ test.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/env bash
+
+# signal whois module that we are testing, this reads data from testdata/<domain>/in
+prepPath()
+{
+    local xpath="$1"
+    TestDataDir=$( realpath "$xpath" )
+    export TEST_WHOIS_PYTHON="$TestDataDir"
+}
+
+get_testdomains()
+{
+    ls "$TestDataDir" |
+    grep -v ".sh"
+}
+
+testOneDomain()
+{
+    domain="$1"
+    [ ! -d "$TestDataDir/$domain" ] && return
+
+    echo "testing: $domain"
+    ./test2.py -d "$domain" >"$TestDataDir/$domain/test.out"
+
+    diff "$TestDataDir/$domain/output" "$TestDataDir/$domain/test.out" |
+    tee "$TestDataDir/$domain/diff.out"
+}
+
+main()
+{
+    prepPath "testdata" # set a default
+    [ -d "$1" ] && { # if a argument and is a dir use that for testing
+        prepPath "$1"
+    }
+
+    get_testdomains |
+    while read line
+    do
+        testOneDomain $(basename $line)
+    done
+}
+
+main $*
