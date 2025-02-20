5dfb3de0d243dd86e344b9d9fc5ff293a080c6a2
mboot-github <130295084+mboot-github@users.noreply.github.com>
2025-02-20 14:48:17 +0100

    update version

--- whoisdomain/version.py
+++ whoisdomain/version.py
@@ -1,2 +1,3 @@
-'''This module only makes the version available for dynamic versioning'''
-VERSION = '1.20250220.1'
+"""This module only makes the version available for dynamic versioning"""
+
+VERSION = "1.20250220.1"
