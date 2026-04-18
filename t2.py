b24eba366ff668cfb570493a56ecf428c3baa4f3
mboot-github <130295084+mboot-github@users.noreply.github.com>
2026-04-18 23:17:55 +0200

    testing

--- t2.py
+++ t2.py
@@ -1,32 +1,34 @@
 import sys
 from typing import Any
 
-import httpx
 import tld
 import whodap
 
 import whoisdomain
 
-client = httpx.Client(follow_redirects=True, timeout=10)
-dns_client = whodap.DNSClient.new_client(client)
 
+class WhoisRdap:
+    def __init__(self) -> None:
+        self.dnsc = whodap.DNSClient.new_client()  # make sure we cache the primary looup for the tld rdap servers
 
-def do_one_domain(domain: str) -> dict[str, Any] | None:
-    d = None
-    try:
-        fld = tld.get_fld(domain, fix_protocol=True, fail_silently=True)
-        a = fld.split(".")
-        dom = ".".join(a[:-1])
-        xtld = a[-1]
-        resp = dns_client.lookup(dom, xtld)
-        d = resp.to_whois_dict()
-    except Exception as e:
-        print(f"Exception: {e}", file=sys.stderr)
+    def do_one_domain(self, domain: str) -> dict[str, Any] | None:
+        try:
+            fld = tld.get_fld(domain, fix_protocol=True, fail_silently=True)
+            a = fld.split(".")
+            dom = ".".join(a[:-1])
+            xtld = a[-1]
+            resp = self.dnsc.lookup(dom, xtld)
+            return resp.to_whois_dict()
+        except Exception as e:
+            print(f"Exception: {e}", file=sys.stderr)
 
-    return d
+        return None
 
 
 def xmain() -> None:
+    rr = {}
+
+    wr = WhoisRdap()
     for k, v in whoisdomain.tldDb.tld_regexpr.ZZ.items():
         server = v.get("_server")
         test = v.get("_test")
@@ -35,11 +37,12 @@ def xmain() -> None:
 
         print("##", k, server, test)
         fld = tld.get_fld(server, fix_protocol=True, fail_silently=True)
-        rr = {}
-        if fld not in rr:
-            rr[fld] = do_one_domain(server)
-            if rr[fld]:
-                print(fld, rr[fld])
+        if fld in rr:
+            continue
+
+        rr[fld] = wr.do_one_domain(server)
+        if rr[fld]:
+            print(fld, rr[fld])
 
 
 xmain()
